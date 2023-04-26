ccaee4f472ad7a308a97353d471ffbf618d3a246
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2023-04-26 12:19:37 +0900

    string boot 생성

---
+++ firstSpringBoot/firstSpringBoot/src/main/java/kr/hs/study/firstSpringBoot/FirstSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.firstSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApplication.class, args);
+	}
+
+}
---
+++ firstSpringBoot/firstSpringBoot/src/test/java/kr/hs/study/firstSpringBoot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.firstSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
