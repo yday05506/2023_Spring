8eb841cd1363086143968b7cd577425be40dec6f
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2023-05-24 12:16:55 +0900

    @PathVariable, @RequestParam

--- GetParameter/src/main/java/kr/hs/study/GetParameter/controller/testController.java
+++ GetParameter/src/main/java/kr/hs/study/GetParameter/controller/testController.java
@@ -2,6 +2,8 @@ package kr.hs.study.GetParameter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -46,6 +48,28 @@ public class testController {
         System.out.println("data1 : " + b);
         for(String str : c) {
             System.out.println("data3 : " + str);
+        }
+
+        return "result";
+    }
+
+    @GetMapping("/test4/{a}/{b}/{c}")
+    public String test4(@PathVariable String a, @PathVariable String b, @PathVariable String c) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        return "result";
+    }
+
+    @GetMapping("/test5")
+    // public String test5(@RequestParam String data1... )처럼 ("data1")을 굳이 안 써도 됨
+    // 쓰는 이유는 잘 보이려고
+    public String test5(@RequestParam("data1") String data1, String data2, String[] data3) {
+        System.out.println(data1);
+        System.out.println(data2);
+        for (String i : data3) {
+            System.out.println(i);
         }
 
         return "result";
